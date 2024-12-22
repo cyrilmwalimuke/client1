46d965f9a13e51dbeb9f70aac80caa69024715b6
cyrilmwalimuke <cyrilmwalimuke@gmail.com>
2024-12-22 15:43:33 +0300

    vercel app

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    historyApiFallback: true, // Ensures routes are handled by React
-  },
+  base:'/'
 })
